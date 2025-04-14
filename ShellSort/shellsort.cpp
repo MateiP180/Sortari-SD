09415994b205e6e2527e931cd1e5a5c743ed1859
MateiP180 <matthew.007.porumboiu@gmail.com>
2025-04-14 22:14:45 +0300

    Update shellsort.cpp

--- ShellSort/shellsort.cpp
+++ ShellSort/shellsort.cpp
@@ -2,9 +2,29 @@
 #include <fstream>
 #include <vector>
 typedef unsigned long long Int;
+
 using namespace std;
 
-void shellSort(vector<long long>& v, Int n)
+
+
+void ReadArray(vector<long long> &arr, size_t &n, char *input_file)
+{
+	ifstream in(input_file);
+
+	in >> n;
+	arr.resize(n);
+
+	for (size_t i = 0; i < n; ++i)
+    {
+		in >> arr[i];
+	}
+
+	in.close();
+	return;
+}
+
+
+void shellSort(vector<long long>& v, long long n)
 {
     for(Int interval = n/2; interval > 0; interval /= 2)
         for(Int i = interval; i<n; i++)
@@ -19,15 +39,22 @@ void shellSort(vector<long long>& v, Int n)
         }
 }
 
-int main()
+void printArray(const vector<long long>& arr)
 {
-    int n;
-    cin>>n;
-    vector <long long> v(n);
-    for(int i=0; i<n; i++)
-        cin>>v[i];
-    shellSort(v, n);
-    for(int i=0; i<n; i++)
-        cout<<v[i]<<" ";
+    for (double x : arr)
+        cout << x << " ";
+    cout << endl;
+}
+
+int main(int argc, char *argv[])
+{
+	size_t n;
+	vector<long long> array;
+    ReadArray(array, n, argv[1]);
+    printArray(array);
+    shellSort(array, n);
+    cout << "Sorted array:\n";
+    printArray(array);
+
     return 0;
 }
